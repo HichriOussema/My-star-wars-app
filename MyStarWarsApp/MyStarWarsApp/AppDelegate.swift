ac30b8a1c8ad7046b10073d360e567cfba95b7ba
oussema_hichri <oussema.mouhib.hichri@gmail.com>
2024-05-23 15:56:06 +0100

    Initial setup: App Delegate with TabBarController

--- MyStarWarsApp/MyStarWarsApp/AppDelegate.swift
+++ MyStarWarsApp/MyStarWarsApp/AppDelegate.swift
@@ -10,16 +10,31 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  var window:UIWindow?
-  
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.makeKeyAndVisible()
-    window?.backgroundColor = .orange
-    window?.rootViewController = ViewController()
-    
-    return true
-  }
+    var window: UIWindow?
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let tabBarController = createTabBarController()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    private func createTabBarController() -> UITabBarController {
+        let tabBarController = UITabBarController()
+        
+        let peopleVC = UINavigationController(rootViewController: UIViewController())
+        peopleVC.tabBarItem = UITabBarItem(title: "People", image: UIImage(systemName: "person.3"), tag: 0)
+        
+        let speciesVC = UINavigationController(rootViewController: UIViewController())
+        speciesVC.tabBarItem = UITabBarItem(title: "Species", image: UIImage(systemName: "leaf.arrow.circlepath"), tag: 1)
+        
+        tabBarController.viewControllers = [peopleVC, speciesVC]
+        
+        return tabBarController
+    }
 }
+
 
